977c37eb7065f8e8927da3b3b13344893c288dd6
vinaythakurcoder <vinaythakur.coder@gmail.com>
2026-01-18 18:52:52 +0530

    add new folder..

--- learning_python_code/basics/file_handling.py
+++ learning_python_code/basics/file_handling.py
@@ -1,5 +1,3 @@
-with open("C:\Users\thaku\OneDrive\Documents\git-clone\python_code\learning_python_code\basics\functions.py", "r+") as f:
-  (f.read())
-
 with open("functions.py", "r") as f:
-    print(f.read())
+    data = f.read()
+print(data)
