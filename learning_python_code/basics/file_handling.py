760eb7ea171c05012195f2b270d68899d37ce567
vinaythakurcoder <vinaythakur.coder@gmail.com>
2026-01-18 23:20:13 +0530

    add file handlingformulas

---
+++ learning_python_code/basics/file_handling.py
@@ -0,0 +1,16 @@
+
+f = open("functions.py", "r")
+line1= f.readline()
+line2= f.readline()
+line3= f.readline()
+line4= f.readline()
+print(line1)
+print(line2)
+print(line3)
+print(line4)
+
+print("ThakurSabji......")
+
+
+with open("functions.py", "r")as f:
+    print(f.read())
