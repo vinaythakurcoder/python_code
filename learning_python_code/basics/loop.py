2cb68611a6771b7e53ce085fa077a71594d311c7
vinaythakurcoder <vinaythakur.coder@gmail.com>
2026-01-28 23:59:24 +0530

    add data's

--- learning_python_code/basics/loop.py
+++ learning_python_code/basics/loop.py
@@ -1,29 +1,29 @@
-# write a program print a natural number 01 to 10.
+# 1. write a program print a natural number 01 to 10.
 
-sir = int(input("Enter a number :"))
+sir = int(input("Enter a number1 :"))
 vinay = 1
 while vinay<=sir:
     print(vinay)
     vinay+=1
 
-# write a program to print a natural number reverse order.
+# 2. write a program to print a natural number reverse order.
 
-sir = int(input("Enter a number :"))
+sir = int(input("Enter a number2 :"))
 vinay = 1
 while sir>=vinay:
     print(sir)
     sir-=1
-# write a program to print squar natural numbers(1 to n).
+# 3. write a program to print squar natural numbers(1 to n).
 
-sir = int(input("Enter a number :"))
+sir = int(input("Enter a number3 :"))
 vinay = 1
 while vinay<=sir:
     print(vinay**2)
     vinay+=1
 
-# write a program to print squar natural numbers(1 to n) in reverse order.
+# 4. write a program to print squar natural numbers(1 to n) in reverse order.
 
-sir = int(input("Enter a number :"))
+sir = int(input("Enter a number4 :"))
 vinay = 1
 while sir>=vinay:
    print(sir**2)
